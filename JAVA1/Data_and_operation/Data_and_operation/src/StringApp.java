43f448ae2790212fee6a93e35ffce3e8c4dde73f
kucucumner <kookh0208@naver.com>
2024-04-09 13:21:58 +0900

    String, Character, New line, Escape について学んだ。

---
+++ JAVA1/Data_and_operation/Data_and_operation/src/StringApp.java
@@ -0,0 +1,11 @@
+public class StringApp {
+    public static void main(String[] args) {
+        System.out.println("Hello World"); // String
+        System.out.println('H'); // Character
+        System.out.println("H");
+
+        System.out.println("Hello \nWorld"); // new line
+        //escape
+        System.out.println("Hello \"World\""); // Hello "World"
+    }
+}
